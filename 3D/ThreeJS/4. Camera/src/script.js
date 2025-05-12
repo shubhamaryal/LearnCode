d684b468f12bd6dd6c72c406846c085a35d980c4
shubhamaryal <aryalshubham62@gmail.com>
2025-05-12 23:23:06 +0545

    practise of 3 js

--- 3D/ThreeJS/4. Camera/src/script.js
+++ 3D/ThreeJS/4. Camera/src/script.js
@@ -1,5 +1,8 @@
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+// console.log(OrbitControls)
 
 /**
  * Cursor
@@ -60,6 +63,15 @@ camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
+
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+// controls.target.y = 1
+// controls.update()
+
+
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
@@ -75,14 +87,14 @@ const tick = () => {
   //   // Update objects
   //   mesh.rotation.y = elapsedTime;
 
-  // Update camera
+  // // Update camera
   //   camera.position.x = cursor.x * 3;
   //   camera.position.y = cursor.y * 3;
   //   camera.lookAt(new THREE.Vector3());
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  camera.position.y = cursor.y * 5;
-  camera.lookAt(mesh.position);
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+  // camera.lookAt(mesh.position);
 
   // Render
   renderer.render(scene, camera);
