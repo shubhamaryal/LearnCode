2e8a36449bb76fe3f19474651556c049fac72f88
shubhamaryal <aryalshubham62@gmail.com>
2025-05-21 12:13:19 +0545

    dsa and 3js

--- 3D/ThreeJS/6. Geometries/src/script.js
+++ 3D/ThreeJS/6. Geometries/src/script.js
@@ -12,8 +12,41 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 4, 4, 4)
+
+// const positionsArray = new Float32Array(9)
+
+// positionsArray[0] = 0
+// positionsArray[1] = 0
+// positionsArray[2] = 0
+
+// positionsArray[3] = 0
+// positionsArray[4] = 1
+// positionsArray[5] = 0
+
+// positionsArray[6] = 1
+// positionsArray[7] = 1
+// positionsArray[8] = 0
+
+const positionsArray = new Float32Array([
+    0, 0, 0, // First vertex
+    0, 1, 0, // Second vertex
+    1, 0, 0 // Third vertex
+])
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+
+const geometry = new THREE.BufferGeometry()
+geometry.setAttribute('position', positionsAttribute)
+
+
+
+
+
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0xff0000,
+    wireframe: true
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
