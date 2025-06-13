3f2b2442ec1f5455aa77bacc40f675ec31eb2972
shubhamaryal <aryalshubham62@gmail.com>
2025-06-13 13:19:38 +0545

    3js ; scroll based animation

---
+++ 3D/ThreeJS/14. Scroll Based Animation/src/script.js
@@ -0,0 +1,191 @@
+import './style.css'
+import * as THREE from 'three'
+import * as dat from 'lil-gui'
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI()
+
+const parameters = {
+    materialColor: '#ffeded'
+}
+
+gui
+    .addColor(parameters, 'materialColor')
+    .onChange(() => {
+        material.color.set(parameters.materialColor)
+    })
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// /**
+//  * Test cube
+//  */
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial({ color: '#ff0000' })
+// )
+// scene.add(cube)
+
+/**
+ * Objects
+ */
+
+// Textures
+const textureLoader = new THREE.TextureLoader()
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+gradientTexture.magFilter = THREE.NearestFilter
+
+// Materials 
+const material = new THREE.MeshToonMaterial({ 
+    color: parameters.materialColor ,
+    gradientMap: gradientTexture
+})
+
+// Meshes
+// const objectDistance  = 2
+const objectDistance  = 4
+
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, 0.4, 16, 60),
+    // new THREE.MeshBasicMaterial({ color: '#ff0000' })
+    material
+)
+
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 2, 32),
+    // new THREE.MeshBasicMaterial({ color: '#ff0000' })
+    material
+)
+
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+    // new THREE.MeshBasicMaterial({ color: '#ff0000' })
+    material
+)
+// mesh1.position.y = 2
+// mesh1.scale.set(0.5, 0.5, 0.5)
+
+// mesh2.visible = false
+
+// mesh3.position.y = -2
+// mesh3.scale.set(0.5, 0.5, 0.5)
+mesh1.position.y = - objectDistance * 0
+mesh2.position.y = - objectDistance * 1
+mesh3.position.y = - objectDistance * 2
+
+mesh1.position.x = 2
+mesh2.position.x = -2
+mesh3.position.x = 2
+
+scene.add(mesh1, mesh2, mesh3)
+
+const sectionMeshes = [mesh1, mesh2, mesh3]
+
+/**
+ * Lights
+ */
+const direcetionalLight = new THREE.DirectionalLight('#ffffff', 1)
+direcetionalLight.position.set(1, 1, 0)
+scene.add(direcetionalLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 6
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true,
+})
+renderer.setClearColor(1)
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Scroll
+ */
+let scrollY = window.scrollY
+
+window.addEventListener('scroll', () =>{
+    scrollY = window.scrollY
+    // console.log(scrollY)
+})
+
+/**
+ * Cursor
+ */
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (event) => {
+    // console.log("mouse moved")
+    // console.log(event.clientX, event.clientY)
+    
+})
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Animate camera
+    camera.position.y = - scrollY / sizes.height * objectDistance
+    // console.log(camera.position.y)
+
+    // Animate meshes
+    for (const mesh of sectionMeshes){
+        mesh.rotation.x = elapsedTime * 0.2
+        mesh.rotation.y = elapsedTime * 0.12
+    }
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
