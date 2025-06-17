2b38d48bcf5eb431a5b91d01f644cf8d4aee119a
shubhamaryal <aryalshubham62@gmail.com>
2025-06-17 23:10:01 +0545

    3js ; physics

--- 3D/ThreeJS/14. Physics/src/script.js
+++ 3D/ThreeJS/14. Physics/src/script.js
@@ -2,6 +2,9 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import CANNON from 'cannon'
+
+// console.log(CANNON)
 
 /**
  * Debug
@@ -31,6 +34,34 @@ const environmentMapTexture = cubeTextureLoader.load([
     '/textures/environmentMaps/0/pz.png',
     '/textures/environmentMaps/0/nz.png'
 ])
+
+/**
+ * Physics
+ */
+// World
+const world = new CANNON.World()
+world.gravity.set(0, -9.82, 0) 
+
+// Sphere
+const sphereShape = new CANNON.Sphere(0.5)
+// radius should be same as the sphere of the three.js
+const sphereBody = new CANNON.Body({
+    mass :1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape: sphereShape,
+})
+world.addBody(sphereBody)
+
+// Floor
+const floorShape = new CANNON.Plane()
+const floorBody = new CANNON.Body()
+floorBody.mass = 0 
+floorBody.addShape(floorShape) 
+floorBody.quaternion.setFromAxisAngle(
+    new CANNON.Vec3(-1, 0, 0),
+    Math.PI * 0.5
+)
+world.addBody(floorBody)
 
 /**
  * Test sphere
@@ -132,11 +163,23 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+let oldElapsedTime = 0
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - oldElapsedTime
+    oldElapsedTime = elapsedTime
+    console.log(deltaTime)
 
+    // Update physics world
+    world.step(1/60, deltaTime, 3)
+
+    // sphere.position.x = sphereBody.position.x 
+    // sphere.position.y = sphereBody.position.y
+    // sphere.position.z = sphereBody.position.z 
+    sphere.position.copy(sphereBody.position)
+    
     // Update controls
     controls.update()
 
