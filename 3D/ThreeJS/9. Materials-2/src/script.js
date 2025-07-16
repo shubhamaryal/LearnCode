70667fb61ff533a030795505614875f91043ef41
shubhamaryal <aryalshubham62@gmail.com>
2025-07-16 23:59:32 +0545

    3js

--- 3D/ThreeJS/9. Materials-2/src/script.js
+++ 3D/ThreeJS/9. Materials-2/src/script.js
@@ -11,6 +11,77 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Texture
+ */
+const textureLoader = new THREE.TextureLoader()
+
+const doorColorTexture = textureLoader.load('./textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('./textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('./textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('./textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('./textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('./textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('./textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('./textures/matcaps/8.png')
+const gradientTexture = textureLoader.load('./textures/gradients/3.png')
+
+doorColorTexture.colorSpace = THREE.SRGBColorSpace
+matcapTexture.colorSpace = THREE.SRGBColorSpace
+
+/**
+ * Objects
+ */
+// Mesh basic material
+// const material = new THREE.MeshBasicMaterial({ map: doorColorTexture })
+// const material = new THREE.MeshBasicMaterial()
+// material.map = doorColorTexture
+// material.color = new THREE.Color('red')
+// material.wireframe = true
+// material.transparent = true
+// material.opacity = 0.2
+// material.alphaMap = doorAlphaTexture
+// material.side = THREE.DoubleSide
+
+// MeshNormal material
+// const material = new THREE.MeshNormalMaterial()
+// material.wireframe = true
+// material.flatShading = true
+
+// Meshmatcap material 
+// const material = new THREE.MeshMatcapMaterial()
+// material.matcap = matcapTexture
+
+// Meshdepth material
+// const material = new THREE.MeshDepthMaterial()
+
+// Meshlambert material
+const material = new THREE.MeshLambertMaterial()
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    material
+)
+sphere.position.x = -1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1, 1),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+    material
+)
+torus.position.x = 1.5
+
+scene.add(sphere, plane, torus)
+
+/**
+ * Lights
+ */
+
+
+/**
  * Sizes
  */
 const sizes = {
@@ -67,6 +138,15 @@ const tick = () =>
 
     // Update controls
     controls.update()
+
+    // Animation 
+    sphere.rotation.x = -0.15 * elapsedTime
+    torus.rotation.x = -0.15 * elapsedTime
+    plane.rotation.x = -0.15 * elapsedTime
+
+    sphere.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+    plane.rotation.y = 0.1 * elapsedTime
 
     // Render
     renderer.render(scene, camera)
