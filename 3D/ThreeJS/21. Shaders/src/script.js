7666c9fda2a65d0adacb635941b0b7df8270f49f
shubhamaryal <aryalshubham62@gmail.com>
2025-08-13 16:04:43 +0545

    3js ; shaders

--- 3D/ThreeJS/21. Shaders/src/script.js
+++ 3D/ThreeJS/21. Shaders/src/script.js
@@ -24,6 +24,7 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const flagTexture = textureLoader.load('/textures/flag-french.jpg')
 
 /**
  * Test mesh
@@ -42,11 +43,14 @@ for(let i =0; i < count ; i++){
 
 geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1)) // the one means that how many values to take for each vertex, usually we do 3 points per vertex but rn it is 1 value per vertex
 
+// console.log(geometry.attributes)
+
 // console.log(geometry) // we can see that we have created a attribute with the name 'aRandom', where 'a' stands for attribute
 
 // Material
 // const material = new THREE.MeshBasicMaterial()
-const material = new THREE.RawShaderMaterial({
+// const material = new THREE.RawShaderMaterial({
+const material = new THREE.ShaderMaterial({
     // vertexShader: '',
     // fragmentShader: ''
 
@@ -73,11 +77,23 @@ const material = new THREE.RawShaderMaterial({
     fragmentShader: testFragmentShader,
     // wireframe: true
     // side : THREE.DoubleSide
-    transparent: true
+    // transparent: true
+    uniforms: {
+        // uFrequency : { value: 10 }
+        // uFrequency : { value: 10, type: 'float' } // old syntax
+        uFrequency : { value: new THREE.Vector2(10, 5) },
+        uTime: { value: 0 },
+        uColor : { value: new THREE.Color('orange') },
+        uTexture: { value: flagTexture },
+    }
 })
+
+gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX')
+gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY')
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
+mesh.scale.y = 2/3
 scene.add(mesh)
 
 /**
@@ -132,6 +148,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update material 
+    material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
--- 3D/ThreeJS/21. Shaders/src/shaders/test/vertex.glsl
+++ 3D/ThreeJS/21. Shaders/src/shaders/test/vertex.glsl
@@ -1,10 +1,18 @@
-uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 modelMatrix;
+// uniform mat4 projectionMatrix;
+// uniform mat4 viewMatrix;
+// uniform mat4 modelMatrix;
 // uniform mat4 modelViewMatrix;
+// uniform float uFrequency;
+uniform vec2 uFrequency;
+uniform float uTime;
 
-attribute vec3 position;
+// attribute vec3 position;
 attribute float aRandom; 
+// attribute vec2 uv;
+
+// varying float vRandom;
+varying vec2 vUv;
+varying float vElevation; 
 
 // float loremIpsum() {
 //     float a = 1.0; 
@@ -26,12 +34,30 @@ void main () {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     // modelPosition.y += 1.0;
     // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1 ;
-    modelPosition.z += aRandom * 0.1;
+    // modelPosition.z += aRandom * 0.1;
+    // modelPosition.z += sin(modelPosition.x * 20.0) * 0.1 ;
+    // modelPosition.z += sin(modelPosition.x * uFrequency) * 0.1 ;
+    // modelPosition.z += sin(modelPosition.x * uFrequency.x) * 0.1 ;
+    // modelPosition.z += sin(modelPosition.y * uFrequency.y) * 0.1 ;
+    // modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1 ;
+    // modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1 ;
+
+    float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1 ;
+    elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1 ;
+
+    modelPosition.z += elevation;
+
+    // modelPosition.y += uTime;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectedPosition;
+
+    // vRandom = aRandom;
+
+    vUv = uv; 
+    vElevation = elevation;
 
     // float result = loremIpsum();
     // float result = loremIpsum(1.0, 2.0);
