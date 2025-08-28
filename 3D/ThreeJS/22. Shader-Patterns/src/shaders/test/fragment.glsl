a3fae4dab0c694abc6763f757afe58c0a1994292
shubhamaryal <aryalshubham62@gmail.com>
2025-08-28 22:40:34 +0545

    3js ; shader patterns

--- 3D/ThreeJS/22. Shader-Patterns/src/shaders/test/fragment.glsl
+++ 3D/ThreeJS/22. Shader-Patterns/src/shaders/test/fragment.glsl
@@ -141,12 +141,40 @@ void main() {
     // float strength = 0.015 / distance(vUv, vec2(0.5));
     // // float strength = 0.015 / distance(vUv, vec2(0.5)) + 0.25 ;
 
-    // Pattern 30
-    vec2 lightUv = vec2(
-        vUv.x * 0.2 + 0.4,
+    // // Pattern 30
+    // vec2 lightUv = vec2(
+    //     vUv.x * 0.1 + 0.45,
+    //     vUv.y * 0.5 + 0.25
+    // );
+    // float strength = 0.015 / distance(lightUv, vec2(0.5));
+
+    // // Pattern 31
+    // vec2 lightUvX = vec2(
+    //     vUv.x * 0.1 + 0.45,
+    //     vUv.y * 0.5 + 0.25
+    // );
+    // float lightX = 0.015 / distance(lightUvX, vec2(0.5));
+
+    // vec2 lightUvY = vec2(
+    //     vUv.y * 0.1 + 0.45,
+    //     vUv.x * 0.5 + 0.25
+    // );
+    // float lightY = 0.015 / distance(lightUvY, vec2(0.5));
+    // float strength = lightX * lightY;
+
+    // Pattern 32
+    vec2 lightUvX = vec2(
+        vUv.x * 0.1 + 0.45,
         vUv.y * 0.5 + 0.25
     );
-    float strength = 0.015 / distance(lightUv, vec2(0.5));
+    float lightX = 0.015 / distance(lightUvX, vec2(0.5));
+
+    vec2 lightUvY = vec2(
+        vUv.y * 0.1 + 0.45,
+        vUv.x * 0.5 + 0.25
+    );
+    float lightY = 0.015 / distance(lightUvY, vec2(0.5));
+    float strength = lightX * lightY;
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
