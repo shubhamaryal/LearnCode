a015389c90f461a41e146a08e37fa71b6e78a98e
shubhamaryal <aryalshubham62@gmail.com>
2026-01-14 22:55:39 +0545

    removed the college folder ; making a new repo for college section

--- _College/5th sem/Web-Class/1/script.js
+++
@@ -1,9 +0,0 @@
-const text = document.getElementById('para')
-console.log(text)
-
-text.textContent = "the text is changed"
-
-const newText = document.createElement('h1')
-newText.textContent = "New heading"
-text.appendChild(newText)
-
--- _College/5th sem/Web-Lab/Lab-4/src/components/Hello.jsx
+++
@@ -1,9 +0,0 @@
-const Hello = ({text}) => {
-  return (
-    <div>
-      {text}
-    </div>
-  );
-};
-
-export default Hello;
