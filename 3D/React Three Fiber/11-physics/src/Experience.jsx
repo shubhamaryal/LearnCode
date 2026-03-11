aa5976abc6a1912e954fa468003e6c846d7900bd
shubhamaryal <aryalshubham62@gmail.com>
2026-03-11 20:54:48 +0545

    r3f; physics

--- 3D/React Three Fiber/11-physics/src/Experience.jsx
+++ 3D/React Three Fiber/11-physics/src/Experience.jsx
@@ -7,8 +7,57 @@ import {
     CuboidCollider,
     BallCollider,
 } from "@react-three/rapier";
+import { useRef } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import * as THREE from "three";
 
 export default function Experience() {
+    const cube = useRef();
+    const twister = useRef();
+
+    const cubeJump = () => {
+        const mass = cube.current.mass();
+        console.log(mass);
+
+        // console.log("jump");
+        // console.log(cube.current);
+        cube.current.applyImpulse({
+            x: 0,
+            // y: 5,
+            y: 5 * mass,
+            z: 0,
+        });
+        cube.current.applyTorqueImpulse({
+            // x: 0,
+            x: Math.random() - 0.5,
+            // y: 1,
+            y: Math.random() - 0.5,
+            // z: 0,
+            z: Math.random() - 0.5,
+        });
+    };
+
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime();
+
+        // const eulerRotation = new THREE.Euler(0, time, 0);
+        const eulerRotation = new THREE.Euler(0, time * 3, 0);
+        const quaternionRotation = new THREE.Quaternion();
+        quaternionRotation.setFromEuler(eulerRotation);
+        twister.current.setNextKinematicRotation(quaternionRotation);
+
+        const angle = time * 0.5;
+        // const x = Math.cos(angle);
+        // const z = Math.sin(angle);
+        const x = Math.cos(angle) * 2;
+        const z = Math.sin(angle) * 2;
+        twister.current.setNextKinematicTranslation({
+            x: x,
+            y: -0.8,
+            z: z,
+        });
+    });
+
     return (
         <>
             <Perf position="top-left" />
@@ -18,18 +67,42 @@ export default function Experience() {
             <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
             <ambientLight intensity={1.5} />
 
-            <Physics>
+            {/* <Physics gravity={[0, -1.6, 0]}> */}
+            <Physics gravity={[0, -10, 0]}>
                 <Debug />
 
                 <RigidBody colliders="ball">
                     <mesh
                         castShadow
                         // position={[-2, 2, 0]}
-                        position={[0, 4, 0]}
+                        // position={[0, 4, 0]}
+                        position={[-1.5, 2, 0]}
                     >
                         <sphereGeometry />
                         <meshStandardMaterial color="orange" />
                     </mesh>
+                </RigidBody>
+
+                <RigidBody
+                    ref={cube}
+                    position={[1.5, 2, 0]}
+                    // gravityScale={0.2}
+                    gravityScale={1}
+                    // restitution={1}
+                    restitution={0}
+                    // friction={0}
+                    friction={0.7}
+                    colliders={false}
+                >
+                    <mesh castShadow onClick={cubeJump}>
+                        <boxGeometry />
+                        <meshStandardMaterial color={"mediumpurple"} />
+                    </mesh>
+                    <CuboidCollider
+                        // mass={0.5}
+                        mass={2}
+                        args={[0.5, 0.5, 0.5]}
+                    />
                 </RigidBody>
 
                 {/* <RigidBody>
@@ -43,15 +116,15 @@ export default function Experience() {
                     </mesh>
                 </RigidBody> */}
 
-                <RigidBody
+                {/* <RigidBody
                     // colliders="ball"
                     // colliders="hull"
                     // colliders="trimesh"
                     colliders={false}
                     position={[0, 1, 0]}
                     rotation={[Math.PI * 0.5, 0, 0]}
-                >
-                    {/* <CuboidCollider
+                > */}
+                {/* <CuboidCollider
                         // args={[1, 1, 1]}
                         args={[1.5, 1.5, 0.5]}
                     />
@@ -62,9 +135,9 @@ export default function Experience() {
                         rotation={[-Math.PI * 0.35, 0, 0]}
                     /> */}
 
-                    <BallCollider args={[1.5]} />
+                {/* <BallCollider args={[1.5]} /> */}
 
-                    <mesh
+                {/* <mesh
                         castShadow
                         // position={[0, 1, 0]}
                         // rotation={[Math.PI * 0.5, 0, 0]}
@@ -72,13 +145,30 @@ export default function Experience() {
                     >
                         <torusGeometry args={[1, 0.5, 16, 32]} />
                         <meshStandardMaterial color={"mediumpurple"} />
-                    </mesh>
-                </RigidBody>
+                    </mesh> */}
+                {/* </RigidBody> */}
 
-                <RigidBody type="fixed">
+                <RigidBody
+                    type="fixed"
+                    // restitution={1}
+                    // friction={0}
+                    friction={0.7}
+                >
                     <mesh receiveShadow position-y={-1.25}>
                         <boxGeometry args={[10, 0.5, 10]} />
                         <meshStandardMaterial color="greenyellow" />
+                    </mesh>
+                </RigidBody>
+
+                <RigidBody
+                    ref={twister}
+                    position={[0, -0.8, 0]}
+                    friction={0}
+                    type="kinematicPosition"
+                >
+                    <mesh castShadow scale={[0.4, 0.4, 3]}>
+                        <boxGeometry />
+                        <meshStandardMaterial color={"red"} />
                     </mesh>
                 </RigidBody>
             </Physics>
